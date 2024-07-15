9a03150bbcf8718b773ffc71a968a1a02601838d
EitanJa24-meet <eitanjacobs7@gmail.com>
2024-07-15 15:01:41 +0300

    Youtube video manager 1.1

--- pythonReview.py
+++ pythonReview.py
@@ -1,22 +1,26 @@
+# main function for the video's details
 def create_youtubevideo (title, description):
  	youtubevideo= {"title": title, "description":description, "likes": 0, "dislikes": 0, "comments":{}}
  	return youtubevideo
 
+# adds to the like counter - function
 def like(youtubevideo):
 	youtubevideo["likes"] +=1
 	return youtubevideo
 
+# adds to the dislike counter - function
 def dislike(youtubevideo):
 
 	youtubevideo["dislikes"] +=1
 	return youtubevideo
 
+# adds a comment - function
 def add_comment(youtubevideo, username, comment_text):
-	if "comments" not in youtubevideo:
-		youtubevideo["comments"] = {}
+	# finds the comment and indside the comments dictionary it checks for a key and adds it if it isnt there and adds the value
 	youtubevideo["comments"][username] = comment_text
 	return youtubevideo
 
+# testing - Omar's youtube video
 Abu_Khaleds_yt_vid = create_youtubevideo("How To", "today we learn how to")
 while Abu_Khaleds_yt_vid["likes"] < 495:
 	Abu_Khaleds_yt_vid = like(Abu_Khaleds_yt_vid)
